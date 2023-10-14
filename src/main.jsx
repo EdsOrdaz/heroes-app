cb751de87aff7ccce60bcd9a82167628a1f1777d
EdsOrdaz <edsordaz@hoitmail.com>
2023-10-14 12:44:00 -0600

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './style.css';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { HeroesApp } from './HeroesApp';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      {/* <BrowserRouter basename={"/heroes-app"}> */}
       <HashRouter>
         <HeroesApp />
       </HashRouter>
-      {/* </BrowserRouter> */}
   </React.StrictMode>,
 )
