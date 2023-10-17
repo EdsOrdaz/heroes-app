84f184597e9593efd1d48326c2ec7efdc8bd39a3
EdsOrdaz <edsordaz@hoitmail.com>
2023-10-16 18:30:44 -0600

    yarn packets

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'animate.css';
 import './style.css';
 import { HashRouter } from 'react-router-dom';
 import { HeroesApp } from './HeroesApp';
